6c5e619b637055dd17f7160a668da23e23b4eafa
Jet Martin <jetamartin@gmail.com>
2018-08-27 16:41:03 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,14 +4,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+},{
+  text: 'Second test todo'
+}];
+
 // Testing lifecyle method
 beforeEach((done) => {
     Todo.remove({}).then( () => {
-      done();
-    });
-    // alternatively you could use the ES6
-// Todo.remove({}).then(() => done()) ;
+      return Todo.insertMany(todos);
+    }).then(() => done());
 });
+
 
 describe('POST /todos', () => {
   // Asych test requires use of done to be passed in call back
@@ -34,7 +39,7 @@ describe('POST /todos', () => {
         // execution given there was an error and prevents logic below from running
         return done(err);
       }
-      Todo.find().then((todos) => {
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -59,12 +64,24 @@ describe('POST /todos', () => {
         return done(err);
       }
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0)
+        expect(todos.length).toBe(2)
         done();
       }).catch((e) => done(e));
-
     });
 
   });
 
+
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
